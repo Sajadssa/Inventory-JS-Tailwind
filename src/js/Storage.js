16ee8cd4ef97dd98167fe74b33e816d36f356788
Sajad Saeedi Azad <55943862+Sajadssa@users.noreply.github.com>
2023-11-02 23:51:48 +0330

    make classes based on OOP

---
+++ src/js/Storage.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    category:"frontend",
+    title: "reactjs",
+    update: "reactjs is powerfull framework for making ui"
+  },
+  {
+    id: 2,
+    idcat: 1,
+    title: "vuejs",
+    description: "vuejs is good framework for making ui"
+
+  }
+];
+
+const categories = [
+  {
+    id: 1,
+    title: "frontend",
+    description:"this is frontend category",
+  },
+  {
+    id: 2,
+    title: "backend",
+    description:"this is backend category",
+  }
+];
+
+export default class Storage{
+  //property
+  //save,delete,update,...
+  //add new category
+  //save category
+  //getAllCategories
+
+
+
+}
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+class App{
+
+}
+
+//target:
+//1.create category
+//2.create product based on category
+//3.execute CRUD In App
+//=> storage class for handle application method
+//product view class
+//categoryViw Class
+//Main and App Class
