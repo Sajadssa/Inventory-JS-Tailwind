da796f423bf24fd6e2011c949ee7735849e5228d
Sajad Saeedi Azad <55943862+Sajadssa@users.noreply.github.com>
2023-11-05 22:07:13 +0330

    save products at localstorage

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -3,20 +3,20 @@ const products = [
     id: 1,
     category:"frontend",
     title: "reactjs",
-    update: "2023-11-03T00:00:00.000Z"
+    createdAt: "2023-11-03T00:00:00.000Z"
   },
   {
     id: 2,
     category: "Backend",
     title: "Node.js",
-    update: "2023-11-05T14:48:00.000"
+    createdAt: "2023-11-05T14:48:00.000"
 
   },
   {
     id:3,
     category: "frontendd",
     title: "vue.js",
-    update: "2023-11-06T14:55:00.000"
+    createdAt: "2023-11-06T14:55:00.000"
     
   }
 ];
@@ -93,6 +93,62 @@ export default class Storage{
     localStorage.setItem('category',JSON.stringify(savedCategories));
 
   }
+
+  // FOR PRODUCTS
+  
+  static getAllProducts() { //products,category=> localStorage=>
+
+    const savedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    //sort=>نزولی=>descnding
+    //1,2,3,4,5,6
+    return sortedProducts = savedProducts.sort((a, b) => {
+      //for sort catagories based on createAt we use from Date Method and take instance from it
+      return new Date(a.createAt) < new Date(b.createAt) ? -1 : 1;
+      //sort based on asc if a<b
+      //sort based on desc if <b
+
+    });
+ 
+    
+  }
+
+  static saveProducts(productsToSave) {
+
+    //FIRST CHECK SAVED CATEGORIES 
+    const savedCategories = localStorage.getAllCategories();
+    //edit...=>save
+    //new=>...save
+    //for check exist products we use Find method
+
+    const exitedItem = savedProducts.find(p => p.id === productsToSave.id);
+    //if an exit go edit products founded
+    if (exitedItem) {
+
+      exitedItem.title = productsToSave.title;
+      exitedItem.quantity = productsToSave.quantity;
+      exitedItem.category = productsToSave.category;
+    }
+    //if have new products
+    else {
+      //we add 2 property push to an object products
+      //push createdAt and Generate new id 
+
+      //new products
+      //we set 2 property for new products
+      productsToSave.id = new Date().getTime();
+      productsToSave.createAt = new Date().toISOString();
+
+      savedProducts.push(productsToSave);
+
+
+    }
+
+    //after edit and created new category we should save in localStorage into savedProducts
+    localStorage.setItem('category', JSON.stringify(savedProducts));
+
+  }
+
+
 
 
 
