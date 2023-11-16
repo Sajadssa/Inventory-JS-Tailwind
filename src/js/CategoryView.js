e4cdffe350dd1ff9bfe3bf11f00ffa898d705e5e
Sajad Saeedi Azad <55943862+Sajadssa@users.noreply.github.com>
2023-11-16 22:19:39 +0330

    add new category and save in categories

---
+++ src/js/CategoryView.js
@@ -0,0 +1,47 @@
+//1. for  add new category we should take title and description from dom and save in categories
+//title,description =>{}=>saveCategory
+
+import Storage from './Storage.js'
+
+const title = document.querySelector("#category-title");
+const description = document.querySelector("#category-description");
+const addNewCategoryBtn= document.querySelector("#add-new-category");
+
+//define class for categoryView that describe properties and method occur in category like add new,delete,update Category
+//because e need category for product so we it default export
+export default class CategoryView{
+  //when create instance from this class go attach eventlistener to button add-new-category -for initialize values here input date by user 
+  constructor() {
+
+    //event object: consist of type event and the element had clicked and this is an object event at summery consist of information about event
+    addNewCategoryBtn.addEventListener("click", (e) => this.addNewCategory(e));
+    //for show allCategories when click button sync
+
+    this.categories = [];
+    
+    
+    
+    
+  }
+
+  //add new category
+  addNewCategory(e) {
+    //for prevent refresh browser
+    e.preventDefault();
+    const title = title.value;
+    const description = description.value;
+    //if title and description are null that mean is falsy
+    if (!title || !description) return;
+    //not  execute code after return
+    //for save new category
+    Storage.saveCategory({ title, description });//3=>4
+   this.categories= Storage.getAllCategories();
+
+    //update DOM: update select option in categories
+
+    
+  }
+
+
+}
+
